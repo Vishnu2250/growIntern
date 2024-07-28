702899f9d37af2cfdebd63238d799747aebc05e9
Pashikanti Vishnu vardhan <149690585+Vishnu2250@users.noreply.github.com>
2024-07-28 17:02:02 +0530

    data.js

---
+++ Disney Hotstar Clone/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+      name: "12th Fail",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/12thfail.webp",
+    },
+    {
+      name: "Badcops",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/badcops.webp",
+    },
+    {
+      name: "Chutney Sambar",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/chutney sambar.jpg",
+    },
+    {
+      name: "Sadak 2",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/sadak2.webp",
+    },
+    {
+      name: "Tadap",
+      des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+      image: "images/tadap.jpeg",
+    },
+  ];
