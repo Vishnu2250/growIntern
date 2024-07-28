3ebe8e33ee2ffc777915d7f635ca50a69ff3f307
Pashikanti Vishnu vardhan <149690585+Vishnu2250@users.noreply.github.com>
2024-07-28 17:53:53 +0530

    script.js

---
+++ Portfolio/script.js
@@ -0,0 +1,15 @@
+/*what i'm*/
+var typeData = new Typed(".role", {
+    strings: [
+        "Front-end Developer",
+        "Web Developer Aspirant",
+        "ex-Intern at CODSOFT",
+        "ex-Intern at OCTANET",
+        "Python Coder",
+        "Investor in Stocks"
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
